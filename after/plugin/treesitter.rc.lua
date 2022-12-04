756df253b42bff5e236308a3215ce8d61447286a
Y.Ninomiya(kadoma) <kadoma.fukakusa248@gmail.com>
2022-12-04 17:20:03 +0900

    fix treesitter

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -2,7 +2,7 @@ local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 treesitter.setup {
-  ensure_installed = "all",
+  ensure_installed = {"vim","dockerfile","fish","typescript","tsx","javascript","json","lua","gitignore","bash","astro","markdown","css","scss","yaml","toml","vue","php"},
   ignore_install = { "html" },
   highlight = {
     enable = true,
