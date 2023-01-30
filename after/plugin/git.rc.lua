cb60ae4d2335f5a33ef0b23c69b9a6cc54e01c42
Y.Ninomiya(kadoma) <kadoma.fukakusa248@gmail.com>
2023-01-30 14:45:03 +0900

    LazyGitのキーバインドを追加

--- after/plugin/git.rc.lua
+++ after/plugin/git.rc.lua
@@ -1,8 +1,10 @@
+-- vim-fugnitive
 vim.cmd([[
-nnoremap <silent> <space>s :Git<CR><C-w>T
-nnoremap <silent> <space>c :Git commit -v<CR><C-w>T
 nnoremap <silent> <space>b :Git blame<CR>
 nnoremap <silent> <space>d :Gvdiffsplit<CR>
 nnoremap <silent> <space>l :Git log --graph<CR><C-w>T
 ]])
 
+local opts = {silent = true, noremap = true}
+-- LazyGitを起動
+vim.keymap.set("n","<space>s",":LazyGit<cr>",opts)
