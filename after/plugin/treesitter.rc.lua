76eff3e21b3dfc8d0a90e1d432593c47f7cd418b
Y.Ninomiya(kadoma) <kadoma.fukakusa248@gmail.com>
2023-02-18 01:43:52 +0900

    add treesitter(markdown-line)

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -2,7 +2,7 @@ local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 treesitter.setup {
-  ensure_installed = {"vim","dockerfile","fish","typescript","tsx","javascript","json","lua","gitignore","bash","astro","markdown","css","scss","yaml","toml","vue","php","html"},
+  ensure_installed = {"vim","dockerfile","fish","typescript","tsx","javascript","json","lua","gitignore","bash","astro","markdown","markdown_inline","css","scss","yaml","toml","vue","php","html"},
   -- ignore_install = { "html" },
   highlight = {
     enable = true,
